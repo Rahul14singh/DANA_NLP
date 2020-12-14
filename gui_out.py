8e3055c72b5373bc8714683979629a4e4f5d3000
RAHUL SINGH <rahulsingh14jan95@gmail.com>
2020-12-14 14:19:11 +0530

    Update gui_out.py

--- gui_out.py
+++ gui_out.py
@@ -45,7 +45,7 @@ class General(QWidget):
     
     def __init__(self):
         super().__init__()
-        self.db=pymysql.connect("192.185.78.17","lifeisio_Rahul","lifeisiot","lifeisio_LIBNLP")
+        self.db=pymysql.connect("#IP ADDRESS FOR DB","#USERID","#PASSWORD","#DB_NAME") # Replace your DB details here
         self.cursor = self.db.cursor()
         print("Database Connected")
         self.READER = MFRC522.MFRC522()
@@ -216,7 +216,7 @@ class General(QWidget):
             bookidsshown=[]
             while continue_reading:
                 if not self.db.open:
-                    self.db=pymysql.connect("192.185.78.17","lifeisio_Rahul","lifeisiot","lifeisio_LIBNLP")
+                    self.db=pymysql.connect("#IP ADDRESS FOR DB","#USERID","#PASSWORD","#DB_NAME") # Replace your DB details here
                     self.cursor = self.db.cursor()
                     print("Database Connected")
                 if val==0:
@@ -342,7 +342,7 @@ class General(QWidget):
         
     def reselectdatabase(self,sql,data):
         if not self.db.open:
-            self.db=pymysql.connect("192.185.78.17","lifeisio_Rahul","lifeisiot","lifeisio_LIBNLP")
+            self.db=pymysql.connect("#IP ADDRESS FOR DB","#USERID","#PASSWORD","#DB_NAME") # Replace your DB details here
             self.cursor = self.db.cursor()
             print("Database Connected")
         try:
